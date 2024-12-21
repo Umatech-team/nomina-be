4416fd110e345dee0cbd5e01091b2626373a89bb
Luan Victor <lvictor.dutra@gmail.com>
2024-12-21 11:26:35 -0300

    feat: implement FindMemberByIdService for retrieving member details by ID

---
+++ src/modules/member/services/FindMemberById.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { Service } from '@shared/core/contracts/Service';
+import { Either, left, right } from '@shared/core/errors/Either';
+import { Member } from '../entities/Member';
+import { EmailAlreadyExistsError } from '../errors/EmailAlreadyExistsError';
+import { MemberNotFoundError } from '../errors/MemberNotFoundError';
+import { MemberRepository } from '../repositories/contracts/MemberRepository';
+
+type Request = TokenPayloadSchema;
+
+type Errors = MemberNotFoundError | EmailAlreadyExistsError;
+
+type Response = {
+  member: Member;
+};
+
+@Injectable()
+export class FindMemberByIdService
+  implements Service<Request, Errors, Response>
+{
+  constructor(private readonly memberRepository: MemberRepository) {}
+
+  async execute({ sub }: Request): Promise<Either<Errors, Response>> {
+    const member = await this.memberRepository.findUniqueById(sub);
+
+    if (!member) {
+      return left(new MemberNotFoundError());
+    }
+
+    return right({
+      member,
+    });
+  }
+}
