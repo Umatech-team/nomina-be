1fdd82cfcabab7b243c37acc10253b8ea09e9834
Luan Victor <lvictor.dutra@gmail.com>
2024-12-21 11:21:40 -0300

    chore: enhance MemberMapper to include additional fields and type safety

--- src/infra/databases/prisma/member/MemberMapper.ts
+++ src/infra/databases/prisma/member/MemberMapper.ts
@@ -1,3 +1,4 @@
+import { PaymentStatus, PlanType, SupportTier } from '@constants/enums';
 import { Member } from '@modules/member/entities/Member';
 import { Member as MemberPrisma, Prisma } from '@prisma/client';
 
@@ -5,19 +6,21 @@ export class MemberMapper {
   static toEntity(raw: MemberPrisma): Member {
     return new Member(
       {
-        name: raw.name,
-        email: raw.email,
-        plan: raw.plan,
-        phone: raw.phone ?? '',
-        planStartDate: raw.planStartDate ?? new Date(),
-        planEndDate: raw.planEndDate,
-        paymentStatus: raw.paymentStatus,
-        renewalDate: raw.renewalDate,
-        timezone: raw.timezone,
-
-        password: raw.password,
         createdAt: raw.createdAt,
         updatedAt: raw.updatedAt,
+        name: raw.name,
+        email: raw.email,
+        phone: raw.phone ?? null,
+        password: raw.password,
+        plan: raw.plan as PlanType,
+        planStartDate: raw.planStartDate as Date,
+        planEndDate: raw.planEndDate,
+        paymentStatus: raw.paymentStatus as PaymentStatus,
+        renewalDate: raw.renewalDate,
+        timezone: raw.timezone,
+        language: raw.language,
+        currency: raw.currency,
+        supportTier: raw.supportTier as SupportTier,
       },
       raw.id,
     );
@@ -25,10 +28,21 @@ export class MemberMapper {
 
   static toPrisma(entity: Member): Prisma.MemberUncheckedCreateInput {
     return {
-      name: entity.name,
-      password: entity.password,
       createdAt: entity.createdAt,
-      updatedAt: entity.updatedAt as Date,
+      updatedAt: entity.updatedAt,
+      name: entity.name,
+      email: entity.email,
+      phone: entity.phone ?? null,
+      password: entity.password,
+      plan: entity.plan as PlanType,
+      planStartDate: entity.planStartDate ?? null,
+      planEndDate: entity.planEndDate,
+      paymentStatus: entity.paymentStatus as PaymentStatus,
+      renewalDate: entity.renewalDate,
+      timezone: entity.timezone,
+      language: entity.language,
+      currency: entity.currency,
+      supportTier: entity.supportTier as SupportTier,
     };
   }
 }
