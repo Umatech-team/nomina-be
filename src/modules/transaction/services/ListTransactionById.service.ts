a7b41014b5df8d394f85d6304bc88fa450cff272
Luan Victor <lvictor.dutra@gmail.com>
2025-02-05 06:42:54 -0300

    feat: add ListTransactionByIdService for retrieving transactions by member ID

---
+++ src/modules/transaction/services/ListTransactionById.service.ts
@@ -0,0 +1,59 @@
+import { MemberRepository } from '@modules/member/repositories/contracts/MemberRepository';
+import { Injectable } from '@nestjs/common';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { Service } from '@shared/core/contracts/Service';
+import { Either, left, right } from '@shared/core/errors/Either';
+import { UnauthorizedError } from '@shared/errors/UnauthorizedError';
+import { ListTransactionsDTO } from '../dto/ListTransactionsDTO';
+import { Transaction } from '../entities/Transaction';
+import { TransactionNotFoundError } from '../errors/TransactionNotFoundError';
+import { TransactionRepository } from '../repositories/contracts/TransactionRepository';
+
+type Request = ListTransactionsDTO & TokenPayloadSchema;
+
+type Errors = TransactionNotFoundError | UnauthorizedError;
+
+type Response = {
+  transaction: Transaction[];
+};
+
+@Injectable()
+export class ListTransactionByIdService
+  implements Service<Request, Errors, Response>
+{
+  constructor(
+    private readonly transactionRepository: TransactionRepository,
+    private readonly memberRepository: MemberRepository,
+  ) {}
+
+  async execute({
+    sub,
+    startDate,
+    endDate,
+    page,
+    pageSize,
+  }: Request): Promise<Either<Errors, Response>> {
+    const member = await this.memberRepository.findUniqueById(sub);
+
+    if (!member) {
+      return left(new TransactionNotFoundError());
+    }
+
+    const transaction =
+      await this.transactionRepository.listTransactionsByMemberId(
+        sub,
+        startDate,
+        endDate,
+        page,
+        pageSize,
+      );
+
+    if (!transaction) {
+      return left(new TransactionNotFoundError());
+    }
+
+    return right({
+      transaction,
+    });
+  }
+}
