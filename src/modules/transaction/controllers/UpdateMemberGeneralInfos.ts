2f54f59f3a561d459a5f44f9521dbe2984a8c9d0
Luan Victor <lvictor.dutra@gmail.com>
2025-02-05 06:41:28 -0300

    feat: add UpdateTransactionController for updating member transaction information

---
+++ src/modules/transaction/controllers/UpdateMemberGeneralInfos.ts
@@ -0,0 +1,42 @@
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { Body, Controller, HttpCode, Patch } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedMember } from '@providers/auth/decorators/CurrentLoggedMember.decorator';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
+import { UpdateTransactionDTO } from '../dto/UpdateTransactionDTO';
+import { UpdateTransactionGateway } from '../gateways/UpdateTransaction.gateway';
+import { TransactionPreviewPresenter } from '../presenters/TransactionPreview.presenter';
+import { UpdateTransactionService } from '../services/UpdateTransaction.service';
+
+@ApiTags('Transaction')
+@Controller('transaction')
+export class UpdateTransactionController {
+  constructor(
+    private readonly updateTransactionService: UpdateTransactionService,
+  ) {}
+
+  @Patch('/update')
+  @HttpCode(statusCode.OK)
+  async handle(
+    @CurrentLoggedMember() { sub }: TokenPayloadSchema,
+    @Body(UpdateTransactionGateway)
+    body: UpdateTransactionDTO,
+  ) {
+    const result = await this.updateTransactionService.execute({
+      ...body,
+      sub,
+    });
+
+    if (result.isLeft()) {
+      return ErrorPresenter.toHTTP(result.value);
+    }
+
+    const { transaction, newBalance } = result.value;
+
+    return {
+      transaction: TransactionPreviewPresenter.toHTTP(transaction),
+      newBalance,
+    };
+  }
+}
