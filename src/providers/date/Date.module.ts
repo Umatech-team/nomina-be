b5ffcb8e9f0e44b0934f850d1c7e8a28d69c8105
Luan Victor <lvictor.dutra@gmail.com>
2025-02-05 06:43:10 -0300

    style: format Date.module.ts for consistent code style

--- src/providers/date/Date.module.ts
+++ src/providers/date/Date.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common'
-import { DateAddition } from './contracts/DateAddition'
-import { DateVerification } from './contracts/DateVerification'
-import { DayJs } from './implementations/Dayjs'
+import { Module } from '@nestjs/common';
+import { DateAddition } from './contracts/DateAddition';
+import { DateVerification } from './contracts/DateVerification';
+import { DayJs } from './implementations/Dayjs';
 
 @Module({
   providers: [
