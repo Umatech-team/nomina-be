ed15f525f3f580eb21785d9ce7c362191d4f4fa6
Luan Victor <lvictor.dutra@gmail.com>
2025-02-05 06:40:38 -0300

    feat: add TransactionRepository to Database module

--- src/infra/databases/Database.module.ts
+++ src/infra/databases/Database.module.ts
@@ -1,9 +1,11 @@
 import { MemberRepository } from '@modules/member/repositories/contracts/MemberRepository';
 import { RefreshTokensRepository } from '@modules/member/repositories/contracts/RefreshTokenRepository';
+import { TransactionRepository } from '@modules/transaction/repositories/contracts/TransactionRepository';
 import { Module } from '@nestjs/common';
 import { MemberRepositoryImplementation } from './prisma/member/MemberRepository';
 import { RefreshTokensRepositoryImplementation } from './prisma/member/RefreshTokensRepositoryImplementation';
 import { PrismaService } from './prisma/prisma.service';
+import { TransactionRepositoryImplementation } from './prisma/transaction/TransactionRepository';
 
 @Module({
   providers: [
@@ -16,7 +18,16 @@ import { PrismaService } from './prisma/prisma.service';
       provide: RefreshTokensRepository,
       useClass: RefreshTokensRepositoryImplementation,
     },
+    {
+      provide: TransactionRepository,
+      useClass: TransactionRepositoryImplementation,
+    },
   ],
-  exports: [PrismaService, MemberRepository, RefreshTokensRepository],
+  exports: [
+    PrismaService,
+    MemberRepository,
+    RefreshTokensRepository,
+    TransactionRepository,
+  ],
 })
 export class DatabaseModule {}
