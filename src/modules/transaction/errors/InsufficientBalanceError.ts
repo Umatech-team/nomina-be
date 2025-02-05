853db1099586a365c1118dc1288e8aa45df7c298
Luan Victor <lvictor.dutra@gmail.com>
2025-02-05 06:42:00 -0300

    feat: add InsufficientBalanceError for handling insufficient balance scenarios

---
+++ src/modules/transaction/errors/InsufficientBalanceError.ts
@@ -0,0 +1,10 @@
+import { ServiceError } from '@shared/core/errors/ServiceError';
+import { statusCode } from '@shared/core/types/statusCode';
+
+export class InsufficientBalanceError extends Error implements ServiceError {
+  statusCode: number = statusCode.CONFLICT;
+
+  constructor() {
+    super('Saldo insuficiente para realizar esta transação.');
+  }
+}
