ee5ea20cc0242c7ac054dd0114093ad05ae61a70
Luan Victor <lvictor.dutra@gmail.com>
2025-02-06 09:51:40 -0300

    feat: add controllers and services for updating member general info and password

--- src/modules/member/Member.module.ts
+++ src/modules/member/Member.module.ts
@@ -6,16 +6,22 @@ import { CreateMemberController } from './controllers/CreateMember.controller';
 import { GetMemberController } from './controllers/GetMember.controller';
 import { LoginMemberController } from './controllers/LoginMember.controller';
 import { RefreshTokenController } from './controllers/RefreshToken.controller';
+import { UpdateMemberGeneralInfosController } from './controllers/UpdateMemberGeneralInfos';
+import { UpdateMemberPasswordController } from './controllers/UpdateMemberPassword';
 import { CreateMemberService } from './services/CreateMember.service';
 import { FindMemberByIdService } from './services/FindMemberById.service';
 import { LoginMemberService } from './services/LoginMember.service';
 import { RefreshTokenService } from './services/RefreshToken.service';
+import { UpdateMemberGeneralInfosService } from './services/UpdateMember.service';
+import { UpdateMemberPasswordService } from './services/UpdateMemberPassword.service';
 
 @Module({
   controllers: [
     CreateMemberController,
     LoginMemberController,
     GetMemberController,
+    UpdateMemberGeneralInfosController,
+    UpdateMemberPasswordController,
     RefreshTokenController,
   ],
   imports: [DatabaseModule, CryptographyModule, DateModule],
@@ -23,6 +29,8 @@ import { RefreshTokenService } from './services/RefreshToken.service';
     CreateMemberService,
     FindMemberByIdService,
     LoginMemberService,
+    UpdateMemberGeneralInfosService,
+    UpdateMemberPasswordService,
     RefreshTokenService,
   ],
 })
