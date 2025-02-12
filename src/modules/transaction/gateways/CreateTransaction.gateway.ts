cc57c8aa56659fb5a73d175067cb127ff10ed2ae
Luan Victor <lvictor.dutra@gmail.com>
2025-02-12 12:10:38 -0300

    feat: update CreateTransaction.gateway to include title and method fields in the transaction schema

--- src/modules/transaction/gateways/CreateTransaction.gateway.ts
+++ src/modules/transaction/gateways/CreateTransaction.gateway.ts
@@ -1,11 +1,14 @@
-import { TransactionType } from '@constants/enums';
+import { TransactionMethod, TransactionType } from '@constants/enums';
 import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
 import { z } from 'zod';
 
 const createTransactionSchema = z.object({
+  title: z.string(),
   type: z.nativeEnum(TransactionType),
+  method: z.nativeEnum(TransactionMethod),
   description: z.string().nullable(),
   category: z.string(),
+  subCategory: z.string(),
   amount: z.number(),
   currency: z.string(),
   date: z.string(),
