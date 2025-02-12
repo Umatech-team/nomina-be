4cd106b4176cee73b8a7a79f7abfa58ec5e1f2b5
Luan Victor <lvictor.dutra@gmail.com>
2025-02-12 12:10:20 -0300

    feat: refactor CreateTransactionDTO to extend TransactionDTO and omit unnecessary fields

--- src/modules/transaction/dto/CreateTransactionDTO.ts
+++ src/modules/transaction/dto/CreateTransactionDTO.ts
@@ -1,29 +1,8 @@
-import { TransactionType } from '@constants/enums';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsDate, IsNumber, IsString } from 'class-validator';
+import { OmitType } from '@nestjs/swagger';
+import { TransactionDTO } from './TransactionDTO';
 
-export class CreateTransactionDTO {
-  @ApiProperty()
-  @IsString()
-  type!: TransactionType;
-
-  @ApiProperty()
-  @IsString()
-  description!: string | null;
-
-  @ApiProperty()
-  @IsString()
-  category!: string;
-
-  @ApiProperty()
-  @IsNumber()
-  amount!: number;
-
-  @ApiProperty()
-  @IsString()
-  currency!: string;
-
-  @ApiProperty()
-  @IsDate()
-  date!: Date;
-}
+export class CreateTransactionDTO extends OmitType(TransactionDTO, [
+  'createdAt',
+  'updatedAt',
+  'memberId',
+]) {}
