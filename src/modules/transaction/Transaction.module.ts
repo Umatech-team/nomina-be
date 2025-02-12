ebf6b8eb16206c61fbd070eae4b8eb004a928d74
Luan Victor <lvictor.dutra@gmail.com>
2025-02-12 12:09:39 -0300

    feat: add controllers and services for monthly summary with percentage and transaction summary by member ID

--- src/modules/transaction/Transaction.module.ts
+++ src/modules/transaction/Transaction.module.ts
@@ -2,17 +2,23 @@ import { DatabaseModule } from '@infra/databases/Database.module';
 import { MemberModule } from '@modules/member/Member.module';
 import { Module } from '@nestjs/common';
 import { CreateTransactionController } from './controllers/CreateTransaction.controller';
+import { FindMonthlySummaryWithPercentageController } from './controllers/FindMonthlySummaryWithPercentage.controller';
 import { FindTransactionController } from './controllers/FindTransaction.controller';
+import { FindTransactionSummaryByMemberIdController } from './controllers/FindTransactionSummaryByMemberId.controller';
 import { ListTransactionController } from './controllers/ListTransaction.controller';
 import { UpdateTransactionController } from './controllers/UpdateMemberGeneralInfos';
 import { CreateTransactionService } from './services/CreateTransaction.service';
+import { FindMonthlySummaryWithPercentageService } from './services/FindMonthSummary.service';
 import { FindTransactionByIdService } from './services/FindTransactionById.service';
+import { FindTransactionSummaryByMemberIdService } from './services/FindTransactionSummaryByMemberId.service';
 import { ListTransactionByIdService } from './services/ListTransactionById.service';
 import { UpdateTransactionService } from './services/UpdateTransaction.service';
 
 @Module({
   controllers: [
     CreateTransactionController,
+    FindMonthlySummaryWithPercentageController,
+    FindTransactionSummaryByMemberIdController,
     FindTransactionController,
     ListTransactionController,
     UpdateTransactionController,
@@ -20,6 +26,8 @@ import { UpdateTransactionService } from './services/UpdateTransaction.service';
   imports: [DatabaseModule, MemberModule],
   providers: [
     CreateTransactionService,
+    FindMonthlySummaryWithPercentageService,
+    FindTransactionSummaryByMemberIdService,
     FindTransactionByIdService,
     ListTransactionByIdService,
     UpdateTransactionService,
