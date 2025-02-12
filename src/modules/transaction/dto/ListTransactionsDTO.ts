d94b390d5d0e36166776b6bc676881fc31884aa6
Luan Victor <lvictor.dutra@gmail.com>
2025-02-12 12:10:24 -0300

    feat: update ListTransactionsDTO to make startDate and endDate optional

--- src/modules/transaction/dto/ListTransactionsDTO.ts
+++ src/modules/transaction/dto/ListTransactionsDTO.ts
@@ -3,18 +3,18 @@ import { IsDate, IsNumber } from 'class-validator';
 
 export class ListTransactionsDTO {
   @ApiProperty()
-  @IsDate()
-  startDate!: Date;
-
-  @ApiProperty()
-  @IsDate()
-  endDate!: Date;
-
-  @ApiProperty()
   @IsNumber()
   page!: number;
 
   @ApiProperty()
   @IsNumber()
   pageSize!: number;
+
+  @ApiProperty()
+  @IsDate()
+  startDate?: Date;
+
+  @ApiProperty()
+  @IsDate()
+  endDate?: Date;
 }
