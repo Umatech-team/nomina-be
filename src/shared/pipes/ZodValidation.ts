3a6c8d70e547a4f5161bc52915f6f0fe0dc9d227
Luan Victor <lvictor.dutra@gmail.com>
2025-02-23 16:14:41 -0300

    feat: add logging to ZodValidationPipe for debugging purposes

--- src/shared/pipes/ZodValidation.ts
+++ src/shared/pipes/ZodValidation.ts
@@ -12,6 +12,7 @@ export class ZodValidationPipe implements PipeTransform {
 
   transform(value: unknown) {
     try {
+      console.log('pipe', value);
       return this.schema.parse(value);
     } catch (err) {
       if (err instanceof ZodError) {
