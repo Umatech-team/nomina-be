1de9c6c286aa8af8a672afc4d4103432cb907537
Luan Victor <lvictor.dutra@gmail.com>
2025-03-02 14:22:07 -0300

    feat: add MonthSummaryWithPercentage value object for financial summary representation

---
+++ src/modules/goal/valueObjects/MonthSumarryWithPercentage.ts
@@ -0,0 +1,58 @@
+import { ValueObject } from '@shared/core/Entities/ValueObject';
+
+export type MonthSumarryWithPercentageType = {
+  memberId: number;
+  id: number;
+  month: Date;
+  totalIncome: number;
+  totalExpense: number;
+  totalInvestments: number;
+  balance: number;
+  percentageChanges: {
+    income: number;
+    expense: number;
+    balance: number;
+  };
+};
+
+export class MonthSumarryWithPercentage extends ValueObject<MonthSumarryWithPercentageType> {
+  constructor(props: MonthSumarryWithPercentageType) {
+    const monthSumarryWithPercentageProps = {
+      ...props,
+    };
+
+    super(monthSumarryWithPercentageProps);
+  }
+
+  get memberId() {
+    return this.props.memberId;
+  }
+
+  get id() {
+    return this.props.id;
+  }
+
+  get month() {
+    return this.props.month;
+  }
+
+  get totalIncome() {
+    return this.props.totalIncome;
+  }
+
+  get totalExpense() {
+    return this.props.totalExpense;
+  }
+
+  get totalInvestments() {
+    return this.props.totalInvestments;
+  }
+
+  get balance() {
+    return this.props.balance;
+  }
+
+  get percentageChanges() {
+    return this.props.percentageChanges;
+  }
+}
