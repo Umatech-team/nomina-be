155bb699b0103c1c2a7b3516d5675c3a6b84ce5b
Luan Victor <lvictor.dutra@gmail.com>
2025-03-02 14:20:41 -0300

    feat: add GoalRepository implementation to DatabaseModule for improved goal management

--- src/infra/databases/Database.module.ts
+++ src/infra/databases/Database.module.ts
@@ -1,7 +1,9 @@
+import { GoalRepository } from '@modules/goal/repositories/contracts/GoalRepository';
 import { MemberRepository } from '@modules/member/repositories/contracts/MemberRepository';
 import { RefreshTokensRepository } from '@modules/member/repositories/contracts/RefreshTokenRepository';
 import { TransactionRepository } from '@modules/transaction/repositories/contracts/TransactionRepository';
 import { Module } from '@nestjs/common';
+import { GoalRepositoryImplementation } from './prisma/goal/GoalRepository';
 import { MemberRepositoryImplementation } from './prisma/member/MemberRepository';
 import { RefreshTokensRepositoryImplementation } from './prisma/member/RefreshTokensRepositoryImplementation';
 import { PrismaService } from './prisma/prisma.service';
@@ -22,12 +24,17 @@ import { TransactionRepositoryImplementation } from './prisma/transaction/Transa
       provide: TransactionRepository,
       useClass: TransactionRepositoryImplementation,
     },
+    {
+      provide: GoalRepository,
+      useClass: GoalRepositoryImplementation,
+    },
   ],
   exports: [
     PrismaService,
     MemberRepository,
     RefreshTokensRepository,
     TransactionRepository,
+    GoalRepository,
   ],
 })
 export class DatabaseModule {}
