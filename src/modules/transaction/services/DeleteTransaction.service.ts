b8a13474f5d70d2e855106cced2faf2ddbc06a20
Luan Victor <lvictor.dutra@gmail.com>
2025-03-21 02:38:37 -0300

    feat: implement DeleteTransactionService for handling transaction deletions

---
+++ src/modules/transaction/services/DeleteTransaction.service.ts
@@ -0,0 +1,57 @@
+import { MemberRepository } from '@modules/member/repositories/contracts/MemberRepository';
+import { Injectable } from '@nestjs/common';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { Service } from '@shared/core/contracts/Service';
+import { Either, left, right } from '@shared/core/errors/Either';
+import { UnauthorizedError } from '@shared/errors/UnauthorizedError';
+import { FindTransactionDTO } from '../dto/FindTransactionDTO';
+import { Transaction } from '../entities/Transaction';
+import { InvalidAmountError } from '../errors/InvalidAmountError';
+import { TransactionNotFoundError } from '../errors/TransactionNotFoundError';
+import { TransactionRepository } from '../repositories/contracts/TransactionRepository';
+
+type Request = FindTransactionDTO & TokenPayloadSchema;
+
+type Errors = UnauthorizedError | InvalidAmountError | TransactionNotFoundError;
+
+type Response = {
+  transaction: Transaction;
+};
+
+@Injectable()
+export class DeleteTransactionService
+  implements Service<Request, Errors, Response>
+{
+  constructor(
+    private readonly transactionRepository: TransactionRepository,
+    private readonly memberRepository: MemberRepository,
+  ) {}
+
+  async execute({
+    sub,
+    transactionId,
+  }: Request): Promise<Either<Errors, Response>> {
+    const member = await this.memberRepository.findUniqueById(sub);
+
+    if (!member) {
+      return left(new UnauthorizedError());
+    }
+
+    const transaction =
+      await this.transactionRepository.findUniqueById(transactionId);
+
+    if (!transaction) {
+      return left(new TransactionNotFoundError());
+    }
+
+    if (transaction.memberId !== sub) {
+      return left(new UnauthorizedError());
+    }
+
+    await this.transactionRepository.delete(transactionId);
+
+    return right({
+      transaction,
+    });
+  }
+}
