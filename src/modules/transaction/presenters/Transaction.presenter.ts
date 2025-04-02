cac65e2f8dd7e43da3bde767729ef9c97c6c512a
Luan Victor <lvictor.dutra@gmail.com>
2025-04-02 15:30:07 -0300

    feat: include title and method in TransactionPresenter output

--- src/modules/transaction/presenters/Transaction.presenter.ts
+++ src/modules/transaction/presenters/Transaction.presenter.ts
@@ -4,6 +4,7 @@ export class TransactionPresenter {
   static toHTTP(transaction: Transaction) {
     return {
       id: transaction.id,
+      title: transaction.title,
       createdAt: transaction.createdAt,
       updatedAt: transaction.updatedAt,
       memberId: transaction.memberId,
@@ -11,6 +12,7 @@ export class TransactionPresenter {
       description: transaction.description,
       category: transaction.category,
       amount: transaction.amount,
+      method: transaction.method,
       currency: transaction.currency,
       date: transaction.date,
     };
