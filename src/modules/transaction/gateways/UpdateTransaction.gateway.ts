40919fc4103725fd11e343e7fd4e5a9b63644ad4
Luan Victor <lvictor.dutra@gmail.com>
2025-05-02 11:09:37 -0300

    feat: add transactionId and title fields to updateTransaction schema

--- src/modules/transaction/gateways/UpdateTransaction.gateway.ts
+++ src/modules/transaction/gateways/UpdateTransaction.gateway.ts
@@ -1,9 +1,12 @@
-import { TransactionType } from '@constants/enums';
+import { TransactionMethod, TransactionType } from '@constants/enums';
 import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
 import { z } from 'zod';
 
 const updateTransaction = z.object({
+  transactionId: z.number(),
+  title: z.string(),
   type: z.nativeEnum(TransactionType),
+  method: z.nativeEnum(TransactionMethod),
   description: z.string().nullable(),
   category: z.string(),
   amount: z.number(),
