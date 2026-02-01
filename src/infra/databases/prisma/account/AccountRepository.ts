d0288999b9d7205c06695a97f33771042e0ae4a3
Luan Victor <lvictor.dutra@gmail.com>
2026-02-01 16:49:58 -0300

    feat: implement findByNameAndWorkspaceId method in AccountRepository

--- src/infra/databases/prisma/account/AccountRepository.ts
+++ src/infra/databases/prisma/account/AccountRepository.ts
@@ -7,6 +7,23 @@ import { AccountMapper } from './AccountMapper';
 @Injectable()
 export class AccountRepositoryImplementation implements AccountRepository {
   constructor(private readonly prisma: PrismaService) {}
+  async findByNameAndWorkspaceId(
+    name: string,
+    workspaceId: string,
+  ): Promise<Account | null> {
+    const account = await this.prisma.account.findFirst({
+      where: {
+        name,
+        workspaceId,
+      },
+    });
+
+    if (!account) {
+      return null;
+    }
+
+    return AccountMapper.toEntity(account);
+  }
 
   async create(account: Account): Promise<Account> {
     const createdAccount = await this.prisma.account.create({
