a45d614c162b625b4a2cd1757a64c3653541b853
Luan Victor <lvictor.dutra@gmail.com>
2026-02-01 18:27:07 -0300

    refactor: reorganize imports and enhance module structure in AppModule

--- src/infra/http/app.module.ts
+++ src/infra/http/app.module.ts
@@ -1,17 +1,46 @@
+import { AccountModule } from '@modules/account/Account.module';
+import { CategoryModule } from '@modules/category/Category.module';
+import { ReportModule } from '@modules/report/Report.module';
 import { TransactionModule } from '@modules/transaction/Transaction.module';
 import { UserModule } from '@modules/user/User.module';
+import { WorkspaceModule } from '@modules/workspace/Workspace.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from '@providers/auth/Auth.module';
+import { JwtAuthGuard } from '@providers/auth/guards/jwtAuth.guard';
+import { RolesGuard } from '@providers/auth/guards/Roles.guard';
 import { CryptographyModule } from '@providers/cryptography/Cryptography.module';
+import { DateModule } from '@providers/date/Date.module';
+import { DatabaseModule } from '../databases/Database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    
+    // Infrastructure
+    DatabaseModule,
     AuthModule,
     CryptographyModule,
+    DateModule,
+    
+    // Domain Modules
     UserModule,
+    WorkspaceModule,
     TransactionModule,
+    AccountModule,
+    CategoryModule,
+    ReportModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
   ],
 })
 export class AppModule {}
