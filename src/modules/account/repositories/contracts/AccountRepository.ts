74c846108b89ede8ce27265b379c0e508307857e
Luan Victor <lvictor.dutra@gmail.com>
2026-02-01 16:50:23 -0300

    feat: add findByNameAndWorkspaceId method to AccountRepository

--- src/modules/account/repositories/contracts/AccountRepository.ts
+++ src/modules/account/repositories/contracts/AccountRepository.ts
@@ -2,4 +2,8 @@ import { Account } from '@modules/account/entities/Account';
 
 export abstract class AccountRepository {
   abstract create(account: Account): Promise<Account>;
+  abstract findByNameAndWorkspaceId(
+    name: string,
+    workspaceId: string,
+  ): Promise<Account | null>;
 }
