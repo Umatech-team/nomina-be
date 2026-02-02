b7580b0aac8db2bf53b865e03d99b84d624ac0cd
Luan Victor <lvictor.dutra@gmail.com>
2026-02-01 21:47:13 -0300

    feat(user): add GetProfileController and service to User module

--- src/modules/user/User.module.ts
+++ src/modules/user/User.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { CryptographyModule } from '@providers/cryptography/Cryptography.module';
 import { DateModule } from '@providers/date/Date.module';
 import { CreateUserController } from './controllers/CreateUser.controller';
+import { GetProfileController } from './controllers/GetProfile.controller';
 import { LoginUserController } from './controllers/LoginUser.controller';
 import { RefreshTokenController } from './controllers/RefreshToken.controller';
 import { CreateUserService } from './services/CreateUser.service';
+import { GetProfileService } from './services/GetProfile.service';
 import { LoginUserService } from './services/LoginUser.service';
 import { RefreshTokenService } from './services/RefreshToken.service';
 import { UpdateUserGeneralInfosService } from './services/UpdateMember.service';
@@ -14,12 +16,14 @@ import { UpdateUserPasswordService } from './services/UpdateMemberPassword.servi
 @Module({
   controllers: [
     CreateUserController,
+    GetProfileController,
     LoginUserController,
     RefreshTokenController,
   ],
   imports: [DatabaseModule, CryptographyModule, DateModule],
   providers: [
     CreateUserService,
+    GetProfileService,
     LoginUserService,
     UpdateUserGeneralInfosService,
     UpdateUserPasswordService,
