e1fc783b0f3d20885cb54a5446896584ae08232c
Luan Victor <lvictor.dutra@gmail.com>
2026-02-01 21:47:17 -0300

    fix(gateway): update validation schema for listWorkspaces to enforce minimum page number

--- src/modules/workspace/gateways/ListWorkspaces.gateway.ts
+++ src/modules/workspace/gateways/ListWorkspaces.gateway.ts
@@ -2,8 +2,8 @@ import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
 import { z } from 'zod';
 
 const listWorkspacesSchema = z.object({
-  page: z.number().int().positive('Página deve ser um número positivo'),
-  pageSize: z
+  page: z.coerce.number().int().min(1, 'Página deve ser no mínimo 1'),
+  pageSize: z.coerce
     .number()
     .int()
     .positive('Tamanho da página deve ser um número positivo')
