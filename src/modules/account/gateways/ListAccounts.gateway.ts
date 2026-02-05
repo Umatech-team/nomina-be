be8daabacbee173c633b871b38af13dde89d5f86
Luan Victor <lvictor.dutra@gmail.com>
2026-02-05 01:16:10 -0300

    refactor(gateway): use z.coerce for number validation in listAccountsSchema

--- src/modules/account/gateways/ListAccounts.gateway.ts
+++ src/modules/account/gateways/ListAccounts.gateway.ts
@@ -2,8 +2,8 @@ import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
 import { z } from 'zod';
 
 const listAccountsSchema = z.object({
-  page: z.number().int().positive('Página deve ser um número positivo'),
-  pageSize: z
+  page: z.coerce.number().int().positive('Página deve ser um número positivo'),
+  pageSize: z.coerce
     .number()
     .int()
     .positive('Tamanho da página deve ser um número positivo')
