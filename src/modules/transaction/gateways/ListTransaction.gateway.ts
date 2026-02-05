38e0a64049173dcffc6507ee7b19023d48d7efe4
Luan Victor <lvictor.dutra@gmail.com>
2026-02-05 01:18:38 -0300

    refactor(gateway): update ListTransactions schema to use coercion for page and pageSize

--- src/modules/transaction/gateways/ListTransaction.gateway.ts
+++ src/modules/transaction/gateways/ListTransaction.gateway.ts
@@ -2,8 +2,12 @@ import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
 import { z } from 'zod';
 
 const listTransactionsSchema = z.object({
-  page: z.number(),
-  pageSize: z.number(),
+  page: z.coerce.number().int().positive('Página deve ser um número positivo'),
+  pageSize: z.coerce
+    .number()
+    .int()
+    .positive('Tamanho da página deve ser um número positivo')
+    .max(100, 'Tamanho da página muito grande'),
   startDate: z.date().optional(),
   endDate: z.date().optional(),
 });
