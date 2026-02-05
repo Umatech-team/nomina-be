102fdfde507b22fcc903bb905861b33ef74399e2
Luan Victor <lvictor.dutra@gmail.com>
2026-02-05 01:18:42 -0300

    refactor(presenter): rename class and update toHTTP method for MonthSummary

--- src/modules/transaction/presenters/MonthlySummary.presenter.ts
+++ src/modules/transaction/presenters/MonthlySummary.presenter.ts
@@ -1,19 +1,14 @@
 import { MoneyUtils } from '@utils/MoneyUtils';
-import { MonthSumarryWithPercentage } from '../valueObjects/MonthSumarryWithPercentage';
+import { MonthSummary } from '../valueObjects/MonthSumarryWithPercentage';
 
-export class MonthlySummaryPresenter {
-  static toHTTP(monthSummary: MonthSumarryWithPercentage) {
+export class MonthSummaryPresenter {
+  static toHTTP(monthSummary: MonthSummary) {
     return {
-      id: monthSummary.id,
-      userId: monthSummary.userId,
       month: monthSummary.month,
       totalIncome: MoneyUtils.centsToDecimal(monthSummary.totalIncome ?? 0),
       totalExpense: MoneyUtils.centsToDecimal(monthSummary.totalExpense ?? 0),
-      totalInvestments: MoneyUtils.centsToDecimal(
-        monthSummary.totalInvestments ?? 0,
-      ),
-      balance: MoneyUtils.centsToDecimal(monthSummary.balance ?? 0),
-      percentageChanges: monthSummary.percentageChanges,
+      currentMonthSaving: monthSummary.rate.currentMonthSaving,
+      previousMonthCompareSaving: monthSummary.rate.previousMonthCompareSaving,
     };
   }
 }
