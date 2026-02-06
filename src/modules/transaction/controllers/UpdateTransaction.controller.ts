3a914349c741754008f1faacad3dd5046bcd2808
Luan Victor <lvictor.dutra@gmail.com>
2026-02-06 01:56:17 -0300

    refactor(controller): add UpdateTransactionController

---
+++ src/modules/transaction/controllers/UpdateTransaction.controller.ts
@@ -0,0 +1,42 @@
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { Body, Controller, HttpCode, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
+import { UpdateTransactionDTO } from '../dto/UpdateTransactionDTO';
+import { UpdateTransactionGateway } from '../gateways/UpdateTransaction.gateway';
+import { TransactionPresenter } from '../presenters/Transaction.presenter';
+import { UpdateTransactionService } from '../services/UpdateTransaction.service';
+
+@ApiTags('Transaction')
+@Controller('transaction')
+export class UpdateTransactionController {
+  constructor(
+    private readonly updateTransactionService: UpdateTransactionService,
+  ) {}
+
+  @Put()
+  @HttpCode(statusCode.OK)
+  async handle(
+    @CurrentLoggedUser() { sub, workspaceId }: TokenPayloadSchema,
+    @Body(UpdateTransactionGateway)
+    body: UpdateTransactionDTO,
+  ) {
+    const result = await this.updateTransactionService.execute({
+      ...body,
+      sub,
+      workspaceId,
+    });
+
+    if (result.isLeft()) {
+      return ErrorPresenter.toHTTP(result.value);
+    }
+
+    const { transaction } = result.value;
+
+    return {
+      transaction: TransactionPresenter.toHTTP(transaction),
+    };
+  }
+}
