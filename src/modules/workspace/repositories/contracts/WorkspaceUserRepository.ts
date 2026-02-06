0ab09e56125ee8ff66fae1dcaa29dd1134e6bf13
Luan Victor <lvictor.dutra@gmail.com>
2026-02-06 01:56:58 -0300

    refactor(repository): update findUniqueById method to return WorkspaceUser instead of Workspace

--- src/modules/workspace/repositories/contracts/WorkspaceUserRepository.ts
+++ src/modules/workspace/repositories/contracts/WorkspaceUserRepository.ts
@@ -9,5 +9,5 @@ export abstract class WorkspaceUserRepository {
   abstract create(workspaceUser: WorkspaceUser): Promise<WorkspaceUser>;
   // abstract update(workspace: Workspace): Promise<void>;
   // abstract delete(id: string): Promise<void>;
-  // abstract findUniqueById(id: string): Promise<Workspace | null>;
+  abstract findUniqueById(id: string): Promise<WorkspaceUser | null>;
 }
