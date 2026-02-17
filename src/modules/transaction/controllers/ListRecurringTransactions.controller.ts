dfce44a8c3f994519bffb2f2067c8edae8a394bf
Luan Victor <lvictor.dutra@gmail.com>
2026-02-17 14:04:29 -0300

    feat(transaction): update ListRecurringTransactionsController to use unified query parameters

--- src/modules/transaction/controllers/ListRecurringTransactions.controller.ts
+++ src/modules/transaction/controllers/ListRecurringTransactions.controller.ts
@@ -4,25 +4,27 @@ import { ApiTags } from '@nestjs/swagger';
 import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
 import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
 import { statusCode } from '@shared/core/types/statusCode';
-import { ListRecurringTransactionsGateway } from '../gateways/ListRecurringTransactions.gateway';
 import { RecurringTransactionPresenter } from '../presenters/RecurringTransaction.presenter';
 import { ListRecurringTransactionsService } from '../services/ListRecurringTransactions.service';
 
+type QueryParams = {
+  page: string;
+  pageSize: string;
+  activeOnly?: string;
+};
 @ApiTags('Recurring Transaction')
 @Controller('transaction')
 export class ListRecurringTransactionsController {
   constructor(private readonly listService: ListRecurringTransactionsService) {}
 
-  @Get()
+  @Get('recurring')
   @HttpCode(statusCode.OK)
   async handle(
     @CurrentLoggedUser() { sub, workspaceId }: TokenPayloadSchema,
-    @Query(ListRecurringTransactionsGateway)
-    @Query('page')
-    page: string,
-    @Query('pageSize') pageSize: string,
-    @Query('activeOnly') activeOnly?: string,
+    @Query() query: QueryParams,
   ) {
+    const { page, pageSize, activeOnly } = query;
+
     const result = await this.listService.execute({
       page: parseInt(page, 10),
       pageSize: parseInt(pageSize, 10),
