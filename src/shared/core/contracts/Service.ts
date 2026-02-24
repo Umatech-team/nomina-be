65152da5755133627a87825310a7b66284980cea
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 20:41:24 -0300

    fix(Service): update K type to include HttpException

--- src/shared/core/contracts/Service.ts
+++ src/shared/core/contracts/Service.ts
@@ -1,9 +1,10 @@
+import { HttpException } from '@nestjs/common';
 import { Either } from '../errors/Either';
 import { ServiceError } from '../errors/ServiceError';
 
 export abstract class Service<
   T = unknown,
-  K extends ServiceError | null = null,
+  K extends HttpException | ServiceError | null = null,
   J = null,
 > {
   abstract execute(props: T, ctx?: unknown): Promise<Either<K, J>>;
