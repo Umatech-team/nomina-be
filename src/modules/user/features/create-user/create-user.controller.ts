d05fcacb8498310dc6240ad18bf6686b68e2c05f
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 20:40:01 -0300

    feat(controller): add CreateUserController for user creation

---
+++ src/modules/user/features/create-user/create-user.controller.ts
@@ -0,0 +1,24 @@
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Public } from '@providers/auth/decorators/IsPublic.decorator';
+import { statusCode } from '@shared/core/types/statusCode';
+import { CreateUserPipe, CreateUserRequest } from './create-user.dto';
+import { CreateUserHandler } from './create-user.handler';
+
+@ApiTags('User')
+@Controller('user')
+export class CreateUserController {
+  constructor(private readonly handler: CreateUserHandler) {}
+
+  @Public()
+  @Post()
+  @HttpCode(statusCode.CREATED)
+  async handle(@Body(CreateUserPipe) body: CreateUserRequest) {
+    const result = await this.handler.execute(body);
+
+    if (result.isLeft()) {
+      return ErrorPresenter.toHTTP(result.value);
+    }
+  }
+}
