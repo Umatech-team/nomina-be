2a5a86cdb7be2fad32a81eb334031ee5d3b0bdf2
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 20:39:04 -0300

    refactor(schema): enhance database schema with new tables and fields for subscriptions and refresh tokens

--- src/infra/databases/drizzle/schema.ts
+++ src/infra/databases/drizzle/schema.ts
@@ -1,4 +1,4 @@
-import { bigint, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { bigint, boolean, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const transactions = pgTable('transactions', {
   id: text('id').primaryKey(),
@@ -23,6 +23,24 @@ export const users = pgTable('users', {
   email: text('email').notNull(),
   passwordHash: text('password_hash').notNull(),
   name: text('name').notNull(),
+  phone: text('phone'),
+  avatarUrl: text('avatar_url'),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
+  updatedAt: timestamp('updated_at', {
+    withTimezone: true,
+    mode: 'date',
+  }).$onUpdate(() => new Date()),
+});
+
+export const accounts = pgTable('accounts', {
+  id: text('id').primaryKey(),
+  workspaceId: text('workspace_id').notNull(),
+  name: text('name').notNull(),
+  type: text('type').notNull(), // 'CASH' | 'CREDIT_CARD' | 'BANK'
+  balance: bigint('balance', { mode: 'number' }).notNull(),
 });
 
 export const workspaces = pgTable('workspaces', {
@@ -34,5 +52,35 @@ export const workspaceUsers = pgTable('workspace_users', {
   id: text('id').primaryKey(),
   workspaceId: text('workspace_id').notNull(),
   userId: text('user_id').notNull(),
-  role: text('role').notNull(), // 'OWNER' | 'ADMIN' | 'MEMBER'
+  isDefault: boolean('is_default').notNull(),
+  role: text('role').notNull(), // 'OWNER' | 'ADMIN' | 'MEMBER',
+  joinedAt: timestamp('joined_at', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
+});
+
+export const subscriptions = pgTable('subscriptions', {
+  id: text('id').primaryKey(),
+  userId: text('user_id').notNull(),
+  planId: text('plan_id').notNull(),
+  status: text('status').notNull(), // 'ACTIVE' | 'INACTIVE' | 'CANCELED'
+  currentPeriodEnd: timestamp('current_period_end', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
+});
+
+export const refreshTokens = pgTable('refresh_tokens', {
+  id: text('id').primaryKey(),
+  userId: text('user_id').notNull(),
+  token: text('token').notNull(),
+  expiresIn: timestamp('expires_in', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
 });
