d18fd0a88459064a78901ff7ca0e544bb34213a1
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 01:19:15 -0300

    refactor(controller): reorganize imports and update CurrentLoggedUser decorator usage

--- src/modules/report/controllers/GetCashFlowEvolution.controller.ts
+++ src/modules/report/controllers/GetCashFlowEvolution.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, HttpCode, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
 import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
 import { CashFlowEvolutionDTO } from '../dto/CashFlowEvolutionDTO';
 import { GetCashFlowEvolutionGateway } from '../gateways/GetCashFlowEvolution.gateway';
 import { CashFlowEvolutionPresenter } from '../presenters/CashFlowEvolutionPresenter';
 import { GetCashFlowEvolutionService } from '../services/GetCashFlowEvolution.service';
-import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
-import { statusCode } from '@shared/core/types/statusCode';
 
 @ApiTags('Report')
 @Controller('report')
@@ -18,7 +18,7 @@ export class GetCashFlowEvolutionController {
   @Get('cash-flow-evolution')
   @HttpCode(statusCode.OK)
   async handle(
-    @CurrentLoggedUser() { sub }: TokenPayloadSchema,
+    @CurrentLoggedUser() { workspaceId }: TokenPayloadSchema,
     @Query(GetCashFlowEvolutionGateway) query: CashFlowEvolutionDTO,
   ) {
     const result = await this.getCashFlowEvolutionService.execute({
