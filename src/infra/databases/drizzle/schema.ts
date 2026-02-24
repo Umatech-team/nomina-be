a42540dc2eebffc025ac4c30bdcf43f5bbeef032
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 01:56:02 -0300

    refactor(schema): add categoryId to transactions table and define categories table

--- src/infra/databases/drizzle/schema.ts
+++ src/infra/databases/drizzle/schema.ts
@@ -3,8 +3,17 @@ import { bigint, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 export const transactions = pgTable('transactions', {
   id: text('id').primaryKey(),
   workspaceId: text('workspace_id').notNull(),
+  categoryId: text('category_id').notNull(),
   amount: bigint('amount', { mode: 'number' }).notNull(),
   date: timestamp('date').notNull(),
   type: text('type').notNull(), // 'INCOME' | 'EXPENSE' | 'TRANSFER'
   status: text('status').notNull(), // 'PENDING' | 'COMPLETED'
+});
+
+export const categories = pgTable('categories', {
+  id: text('id').primaryKey(),
+  name: text('name').notNull(),
+  type: text('type').notNull(), // 'INCOME' | 'EXPENSE'
+  isSystemCategory: text('is_system_category').notNull(),
+  parentId: text('parent_id'),
 });
