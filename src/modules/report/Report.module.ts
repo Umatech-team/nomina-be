b52bc01808e8280e3f5d071636eadf36af64fb52
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 14:45:21 -0300

    refactor(report): reorganize controllers and providers in ReportModule

--- src/modules/report/Report.module.ts
+++ src/modules/report/Report.module.ts
@@ -1,13 +1,23 @@
 import { DatabaseModule } from '@infra/databases/Database.module';
 import { Module } from '@nestjs/common';
-import { CashFlowEvolutionController } from './features/cash-flow-evolution/cash-flow-evolution.controller';
-import { CashFlowEvolutionHandler } from './features/cash-flow-evolution/cash-flow-evolution.handler';
+import { BalanceEvolutionController } from './features/get-balance-evolution/get-balance-evolution.controller';
+import { BalanceEvolutionHandler } from './features/get-balance-evolution/get-balance-evolution.handler';
+import { CashFlowEvolutionController } from './features/get-cash-flow-evolution/cash-flow-evolution.controller';
+import { CashFlowEvolutionHandler } from './features/get-cash-flow-evolution/cash-flow-evolution.handler';
 import { GetExpensesByCategoryController } from './features/get-expenses-by-category/get-expenses-by-category.controller';
 import { GetExpensesByCategoryHandler } from './features/get-expenses-by-category/get-expenses-by-category.handler';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CashFlowEvolutionController, GetExpensesByCategoryController],
-  providers: [CashFlowEvolutionHandler, GetExpensesByCategoryHandler],
+  controllers: [
+    CashFlowEvolutionController,
+    GetExpensesByCategoryController,
+    BalanceEvolutionController,
+  ],
+  providers: [
+    CashFlowEvolutionHandler,
+    GetExpensesByCategoryHandler,
+    BalanceEvolutionHandler,
+  ],
 })
 export class ReportModule {}
