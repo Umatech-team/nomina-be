6d473f0137ca5350eb76f615fbda272d0cb1b059
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 14:44:53 -0300

    refactor(category): remove unused icon and color fields from CategoryMapper

--- src/infra/databases/prisma/category/CategoryMapper.ts
+++ src/infra/databases/prisma/category/CategoryMapper.ts
@@ -8,8 +8,6 @@ export class CategoryMapper {
       {
         workspaceId: raw.workspaceId,
         name: raw.name,
-        icon: raw.icon,
-        color: raw.color,
         type: raw.type as TransactionType,
         parentId: raw.parentId,
         isSystemCategory: raw.isSystemCategory,
@@ -23,8 +21,6 @@ export class CategoryMapper {
       id: entity.id,
       workspaceId: entity.workspaceId,
       name: entity.name,
-      icon: entity.icon,
-      color: entity.color,
       type: entity.type,
       parentId: entity.parentId,
       isSystemCategory: entity.isSystemCategory,
