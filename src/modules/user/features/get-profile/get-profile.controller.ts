26988a04650939acb4a96d82f721c0f3c3482bbc
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 20:40:11 -0300

    feat(controller): add GetProfileController for user profile retrieval

---
+++ src/modules/user/features/get-profile/get-profile.controller.ts
@@ -0,0 +1,25 @@
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
+import { GetProfileHandler } from './get-profile.handler';
+
+@ApiTags('Auth')
+@Controller('auth')
+export class GetProfileController {
+  constructor(private readonly handler: GetProfileHandler) {}
+
+  @Get('/me')
+  @HttpCode(statusCode.OK)
+  async handle(@CurrentLoggedUser() sub: TokenPayloadSchema) {
+    const data = await this.handler.execute(sub);
+
+    if (data.isLeft()) {
+      return ErrorPresenter.toHTTP(data.value);
+    }
+
+    return { data: data.value };
+  }
+}
