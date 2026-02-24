685d3cb856c8bf7ed1acf3b025ad0b0d5fdaabd6
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 01:19:19 -0300

    feat(controller): add CashFlowEvolutionController for cash flow evolution reports

---
+++ src/modules/report/features/cash-flow-evolution/cash-flow-evolution.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, HttpCode, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
+import {
+  CashFlowEvolutionPipe,
+  CashFlowEvolutionRequest,
+} from './cash-flow-evolution.dto';
+import { CashFlowEvolutionHandler } from './cash-flow-evolution.handler';
+
+@ApiTags('Report')
+@Controller('report')
+export class CashFlowEvolutionController {
+  constructor(private readonly handler: CashFlowEvolutionHandler) {}
+
+  @Get('cash-flow-evolution')
+  @HttpCode(statusCode.OK)
+  async handle(
+    @CurrentLoggedUser() { workspaceId }: TokenPayloadSchema,
+    @Query(CashFlowEvolutionPipe) query: CashFlowEvolutionRequest,
+  ) {
+    const data = await this.handler.execute({
+      workspaceId,
+      ...query,
+    });
+
+    return { data };
+  }
+}
