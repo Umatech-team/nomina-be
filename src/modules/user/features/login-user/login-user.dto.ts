4bdf21c8715f6d80139059aa1156da7d2bb9375c
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 20:40:21 -0300

    feat(dto): add LoginUser DTO and validation schema for user authentication

---
+++ src/modules/user/features/login-user/login-user.dto.ts
@@ -0,0 +1,10 @@
+import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
+import { z } from 'zod';
+
+const loginUserSchema = z.object({
+  email: z.string().trim().email('E-mail inválido'),
+  password: z.string().trim().min(8).max(255),
+});
+
+export type LoginUserRequest = z.infer<typeof loginUserSchema>;
+export const LoginUserPipe = new ZodValidationPipe(loginUserSchema);
