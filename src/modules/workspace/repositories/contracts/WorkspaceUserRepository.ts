882bd62614803dda829ed659dc5d136e84342b52
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 22:38:18 -0300

    refactor(repository): update findDefaultByUserId return type and add additional methods in WorkspaceUserRepository

--- src/modules/workspace/repositories/contracts/WorkspaceUserRepository.ts
+++ src/modules/workspace/repositories/contracts/WorkspaceUserRepository.ts
@@ -1,13 +1,26 @@
-import { WorkspaceMemberWithDetails } from '@infra/databases/prisma/workspace/WorkspaceUserMapper';
 import { WorkspaceUser } from '@modules/workspace/entities/WorkspaceUser';
 
 export abstract class WorkspaceUserRepository {
-  abstract findDefaultByUserId(
-    userId: string,
-  ): Promise<WorkspaceMemberWithDetails | null>;
+  abstract findDefaultByUserId(userId: string): Promise<{
+    member: WorkspaceUser;
+    workspaceName: string;
+  } | null>;
 
   abstract create(workspaceUser: WorkspaceUser): Promise<WorkspaceUser>;
-  // abstract update(workspace: Workspace): Promise<void>;
-  // abstract delete(id: string): Promise<void>;
   abstract findUniqueById(id: string): Promise<WorkspaceUser | null>;
+  abstract addUser(workspaceUser: WorkspaceUser): Promise<WorkspaceUser>;
+  abstract removeUser(id: string): Promise<void>;
+  abstract updateUser(workspaceUser: WorkspaceUser): Promise<WorkspaceUser>;
+  abstract findUsersByWorkspaceId(
+    workspaceId: string,
+    page: number,
+    limit: number,
+  ): Promise<{ workspaceUsers: WorkspaceUser[]; total: number }>;
+
+  abstract findUserByWorkspaceAndUserId(
+    workspaceId: string,
+    userId: string,
+  ): Promise<WorkspaceUser | null>;
+
+  abstract findUserById(id: string): Promise<WorkspaceUser | null>;
 }
