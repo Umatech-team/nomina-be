a9ed21c351d139a0c0aa833625b4baf98611ee72
Luan Victor <lvictor.dutra@gmail.com>
2026-02-25 17:16:51 -0300

    feat(recurring-transaction): implement RecurringTransactionMapper for domain and database transformations

---
+++ src/infra/databases/drizzle/mappers/recurring-transaction.mapper.ts
@@ -0,0 +1,56 @@
+import { RecurrenceFrequency, TransactionType } from '@constants/enums';
+import * as schema from '@infra/databases/drizzle/schema';
+import { RecurringTransaction } from '@modules/transaction/entities/RecurringTransaction';
+
+type RecurringTransactionDrizzle =
+  typeof schema.recurringTransactions.$inferSelect;
+type RecurringTransactionDrizzleInsert =
+  typeof schema.recurringTransactions.$inferInsert;
+
+export class RecurringTransactionMapper {
+  static toDomain(raw: RecurringTransactionDrizzle): RecurringTransaction {
+    const result = RecurringTransaction.create(
+      {
+        accountId: raw.accountId,
+        amount: BigInt(raw.amount),
+        categoryId: raw.categoryId,
+        description: raw.description,
+        workspaceId: raw.workspaceId,
+        frequency: raw.frequency as RecurrenceFrequency,
+        startDate: raw.startDate,
+        endDate: raw.endDate,
+        active: raw.active,
+        type: raw.type as TransactionType,
+        interval: raw.interval,
+        lastGenerated: raw.lastGenerated,
+      },
+      raw.id,
+    );
+
+    if (result.isLeft()) {
+      throw result.value;
+    }
+
+    return result.value;
+  }
+
+  static toDatabase(
+    entity: RecurringTransaction,
+  ): RecurringTransactionDrizzleInsert {
+    return {
+      id: entity.id,
+      accountId: entity.accountId,
+      amount: Number(entity.amount),
+      categoryId: entity.categoryId,
+      description: entity.description,
+      workspaceId: entity.workspaceId,
+      frequency: entity.frequency,
+      startDate: entity.startDate,
+      endDate: entity.endDate,
+      active: entity.active,
+      type: entity.type,
+      interval: entity.interval,
+      lastGenerated: entity.lastGenerated,
+    };
+  }
+}
