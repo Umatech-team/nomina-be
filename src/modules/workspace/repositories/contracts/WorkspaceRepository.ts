91edb278d9d19ab367689196b2bfc0f2ec2dda22
Luan Victor <lvictor.dutra@gmail.com>
2026-02-24 22:38:15 -0300

    refactor(repository): remove unused user management methods from WorkspaceRepository

--- src/modules/workspace/repositories/contracts/WorkspaceRepository.ts
+++ src/modules/workspace/repositories/contracts/WorkspaceRepository.ts
@@ -22,22 +22,5 @@ export abstract class WorkspaceRepository {
       role: UserRole;
       isDefault: boolean;
     }>;
-    total: number;
   }>;
-
-  abstract addUser(workspaceUser: WorkspaceUser): Promise<WorkspaceUser>;
-  abstract removeUser(id: string): Promise<void>;
-  abstract updateUser(workspaceUser: WorkspaceUser): Promise<WorkspaceUser>;
-  abstract findUsersByWorkspaceId(
-    workspaceId: string,
-    page: number,
-    limit: number,
-  ): Promise<{ workspaceUsers: WorkspaceUser[]; total: number }>;
-
-  abstract findUserByWorkspaceAndUserId(
-    workspaceId: string,
-    userId: string,
-  ): Promise<WorkspaceUser | null>;
-
-  abstract findUserById(id: string): Promise<WorkspaceUser | null>;
 }
