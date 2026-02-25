1e7ecfb09ce36e17ce7f2d34bf5dd19bdfe06a24
Luan Victor <lvictor.dutra@gmail.com>
2026-02-25 18:22:55 -0300

    feat(transaction): add FindTransactionController for handling transaction retrieval

---
+++ src/modules/transaction/features/find-transaction/find-transaction.controller.ts
@@ -0,0 +1,35 @@
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { Controller, Get, HttpCode, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
+import {
+  FindTransactionPipe,
+  FindTransactionRequest,
+} from './find-transaction.dto';
+import { FindTransactionByIdHandler } from './find-transaction.handle';
+
+@ApiTags('Transaction')
+@Controller('transaction')
+export class FindTransactionController {
+  constructor(private readonly handler: FindTransactionByIdHandler) {}
+
+  @Get()
+  @HttpCode(statusCode.OK)
+  async handle(
+    @CurrentLoggedUser() { sub }: TokenPayloadSchema,
+    @Query(FindTransactionPipe) { transactionId }: FindTransactionRequest,
+  ) {
+    const data = await this.handler.execute({
+      sub,
+      transactionId,
+    });
+
+    if (data.isLeft()) {
+      return ErrorPresenter.toHTTP(data.value);
+    }
+
+    return { data: data.value };
+  }
+}
