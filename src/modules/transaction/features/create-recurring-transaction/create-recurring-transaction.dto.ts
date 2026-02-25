e02d0ff240e6f62ca78fd8a007557083497f6267
Luan Victor <lvictor.dutra@gmail.com>
2026-02-25 17:34:59 -0300

    feat(recurring-transaction): add DTO and validation pipe for creating recurring transactions

---
+++ src/modules/transaction/features/create-recurring-transaction/create-recurring-transaction.dto.ts
@@ -0,0 +1,51 @@
+import { RecurrenceFrequency, TransactionType } from '@constants/enums';
+import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
+import { z } from 'zod';
+
+const createRecurringTransactionSchema = z.object({
+  accountId: z.string().uuid('ID da conta inválido'),
+  categoryId: z.string().uuid('ID da categoria inválido'),
+  description: z.string().min(1, 'Descrição é obrigatória'),
+  amount: z.coerce.bigint().positive('Valor deve ser positivo'),
+  frequency: z.nativeEnum(RecurrenceFrequency),
+  type: z.nativeEnum(TransactionType),
+  interval: z.coerce
+    .number()
+    .int()
+    .positive('Intervalo deve ser um número positivo')
+    .optional(),
+  startDate: z
+    .string()
+    .refine(
+      (dateString) => {
+        const date = new Date(dateString);
+        return !isNaN(date.getTime());
+      },
+      {
+        message: 'Data de início deve estar em um formato válido',
+      },
+    )
+    .transform((dateString) => new Date(dateString)),
+  endDate: z
+    .string()
+    .refine(
+      (dateString) => {
+        const date = new Date(dateString);
+        return !isNaN(date.getTime());
+      },
+      {
+        message: 'Data de fim deve estar em um formato válido',
+      },
+    )
+    .transform((dateString) => new Date(dateString))
+    .optional()
+    .nullable(),
+  active: z.boolean().optional(),
+});
+
+export type CreateRecurringTransactionRequest = z.infer<
+  typeof createRecurringTransactionSchema
+>;
+export const CreateRecurringTransactionPipe = new ZodValidationPipe(
+  createRecurringTransactionSchema,
+);
