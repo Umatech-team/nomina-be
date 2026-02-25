d898229cf2c0f5d835fab60d0569d322f9b01d4a
Luan Victor <lvictor.dutra@gmail.com>
2026-02-25 15:42:29 -0300

    refactor(auth): replace LoginUserService with LoginUserHandler in LoginUserController

--- src/modules/user/features/login-user/login-user.controller.ts
+++ src/modules/user/features/login-user/login-user.controller.ts
@@ -1,21 +1,21 @@
 import { ErrorPresenter } from '@infra/presenters/Error.presenter';
-import { LoginUserService } from '@modules/user/services/LoginUser.service';
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Public } from '@providers/auth/decorators/IsPublic.decorator';
 import { statusCode } from '@shared/core/types/statusCode';
 import { LoginUserPipe, LoginUserRequest } from './login-user.dto';
+import { LoginUserHandler } from './login-user.handler';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class LoginUserController {
-  constructor(private readonly loginUserService: LoginUserService) {}
+  constructor(private readonly handler: LoginUserHandler) {}
 
   @Public()
   @Post('login')
   @HttpCode(statusCode.OK)
   async handle(@Body(LoginUserPipe) body: LoginUserRequest) {
-    const data = await this.loginUserService.execute(body);
+    const data = await this.handler.execute(body);
 
     if (data.isLeft()) {
       return ErrorPresenter.toHTTP(data.value);
