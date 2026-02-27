eeebbbf86bea479c076ebb99363b686882415382
Luan Victor <lvictor.dutra@gmail.com>
2026-02-26 22:18:00 -0300

    feat(transaction): implement DeleteTransactionController for transaction deletion

---
+++ src/modules/transaction/features/delete-transaction/delete-transaction.controller.ts
@@ -0,0 +1,36 @@
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { Controller, Delete, HttpCode, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
+import {
+  DeleteTransactionPipe,
+  DeleteTransactionRequest,
+} from './delete-transaction.dto';
+import { DeleteTransactionHandler } from './delete-transaction.handler';
+
+@ApiTags('Transaction')
+@Controller('transaction')
+export class DeleteTransactionController {
+  constructor(
+    private readonly deleteTransactionService: DeleteTransactionHandler,
+  ) {}
+
+  @Delete()
+  @HttpCode(statusCode.OK)
+  async handle(
+    @CurrentLoggedUser() { sub, workspaceId }: TokenPayloadSchema,
+    @Query(DeleteTransactionPipe) { transactionId }: DeleteTransactionRequest,
+  ) {
+    const result = await this.deleteTransactionService.execute({
+      transactionId,
+      sub,
+      workspaceId,
+    });
+
+    if (result.isLeft()) {
+      return ErrorPresenter.toHTTP(result.value);
+    }
+  }
+}
