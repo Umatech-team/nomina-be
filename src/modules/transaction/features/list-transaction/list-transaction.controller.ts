0c3528e820e6622644757f623d9ed8921a017289
Luan Victor <lvictor.dutra@gmail.com>
2026-02-26 21:19:07 -0300

    feat(transaction): add ListTransactionController for listing transactions

---
+++ src/modules/transaction/features/list-transaction/list-transaction.controller.ts
@@ -0,0 +1,58 @@
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { TransactionPreviewPresenter } from '@modules/transaction/presenters/TransactionPreview.presenter';
+import { Controller, Get, HttpCode, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
+import {
+  ListTransactionsPipe,
+  ListTransactionsRequest,
+} from './list-transaction.dto';
+import { ListTransactionByIdHandler } from './list-transaction.handler';
+
+@ApiTags('Transaction')
+@Controller('transaction')
+export class ListTransactionController {
+  constructor(private readonly handler: ListTransactionByIdHandler) {}
+
+  @Get('list')
+  @HttpCode(statusCode.OK)
+  async handle(
+    @CurrentLoggedUser() { sub, workspaceId }: TokenPayloadSchema,
+    @Query(ListTransactionsPipe)
+    {
+      page,
+      pageSize,
+      startDate,
+      endDate,
+      type,
+      categoryId,
+      accountId,
+      description,
+      status,
+    }: ListTransactionsRequest,
+  ) {
+    const data = await this.handler.execute({
+      page,
+      pageSize,
+      startDate,
+      endDate,
+      sub,
+      workspaceId,
+      type,
+      categoryId,
+      accountId,
+      description,
+      status,
+    });
+
+    if (data.isLeft()) {
+      return ErrorPresenter.toHTTP(data.value);
+    }
+
+    return {
+      data: data.value.map(TransactionPreviewPresenter.toHTTP),
+    };
+  }
+}
