c7d121a70db5e1683f3be7d1c6a09aba77bae74d
Luan Victor <lvictor.dutra@gmail.com>
2026-02-26 22:46:55 -0300

    feat(report): add MonthSummaryPresenter for formatting monthly summary data

---
+++ src/modules/report/presenters/MonthlySummary.presenter.ts
@@ -0,0 +1,14 @@
+import { MoneyUtils } from '@utils/MoneyUtils';
+import { MonthSummary } from '../../transaction/valueObjects/MonthSumarryWithPercentage';
+
+export class MonthSummaryPresenter {
+  static toHTTP(monthSummary: MonthSummary) {
+    return {
+      month: monthSummary.month,
+      totalIncome: MoneyUtils.centsToDecimal(monthSummary.totalIncome ?? 0),
+      totalExpense: MoneyUtils.centsToDecimal(monthSummary.totalExpense ?? 0),
+      currentMonthSaving: monthSummary.rate.currentMonthSaving,
+      previousMonthCompareSaving: monthSummary.rate.previousMonthCompareSaving,
+    };
+  }
+}
