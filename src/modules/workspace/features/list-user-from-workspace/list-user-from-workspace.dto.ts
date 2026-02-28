3bc3c9d09b022be62107841d91276d13193e7247
Luan Victor <lvictor.dutra@gmail.com>
2026-02-28 13:11:36 -0300

    feat(workspace): add ListWorkspaceUsersPipe and schema for validating workspace user requests

---
+++ src/modules/workspace/features/list-user-from-workspace/list-user-from-workspace.dto.ts
@@ -0,0 +1,19 @@
+import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
+import { z } from 'zod';
+
+const listWorkspaceUsersSchema = z.object({
+  workspaceId: z.string().uuid('ID do workspace inválido'),
+  page: z.coerce.number().int().positive('Página deve ser um número positivo'),
+  pageSize: z.coerce
+    .number()
+    .int()
+    .positive('Tamanho da página deve ser um número positivo')
+    .max(100, 'Tamanho da página muito grande'),
+});
+
+export const ListWorkspaceUsersPipe = new ZodValidationPipe(
+  listWorkspaceUsersSchema,
+);
+export type ListWorkspaceUsersRequest = z.infer<
+  typeof listWorkspaceUsersSchema
+>;
