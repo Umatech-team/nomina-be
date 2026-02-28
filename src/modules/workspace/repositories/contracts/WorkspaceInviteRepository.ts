b96cc4ea3ea5d0a89b23bb701dd871c63b00c7f6
Luan Victor <lvictor.dutra@gmail.com>
2026-02-28 13:11:51 -0300

    feat(workspace): update create method in WorkspaceInviteRepository to return WorkspaceInvite

--- src/modules/workspace/repositories/contracts/WorkspaceInviteRepository.ts
+++ src/modules/workspace/repositories/contracts/WorkspaceInviteRepository.ts
@@ -1,7 +1,7 @@
 import { WorkspaceInvite } from '@modules/workspace/entities/WorkspaceInvite';
 
 export abstract class WorkspaceInviteRepository {
-  abstract create(invite: WorkspaceInvite): Promise<void>;
+  abstract create(invite: WorkspaceInvite): Promise<WorkspaceInvite>;
   abstract findByCode(code: string): Promise<WorkspaceInvite | null>;
   abstract markAsUsed(id: string, usedByUserId: string): Promise<void>;
 }
