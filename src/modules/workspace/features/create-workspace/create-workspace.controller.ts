16ac15101774369e71cf98227c49ca39a6d33b6f
Luan Victor <lvictor.dutra@gmail.com>
2026-02-28 13:10:55 -0300

    feat(workspace): add CreateWorkspaceController for handling workspace creation

---
+++ src/modules/workspace/features/create-workspace/create-workspace.controller.ts
@@ -0,0 +1,43 @@
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { CheckLimit } from '@modules/subscription/decorators/CheckLimit.decorator';
+import { SubscriptionLimitsGuard } from '@modules/subscription/guards/SubscriptionLimits.guard';
+import { ResourceType } from '@modules/subscription/services/CheckSubscriptionLimits.service';
+import { WorkspacePresenter } from '@modules/workspace/presenters/Workspace.presenter';
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
+import {
+  CreateWorkspacePipe,
+  CreateWorkspaceRequest,
+} from './create-workspace.dto';
+import { CreateWorkspaceHandler } from './create-workspace.handler';
+
+@ApiTags('Workspace')
+@Controller('workspace')
+export class CreateWorkspaceController {
+  constructor(private readonly handler: CreateWorkspaceHandler) {}
+
+  @Post()
+  @HttpCode(statusCode.CREATED)
+  @UseGuards(SubscriptionLimitsGuard)
+  @CheckLimit(ResourceType.WORKSPACE)
+  async handle(
+    @CurrentLoggedUser() { sub }: TokenPayloadSchema,
+    @Body(CreateWorkspacePipe) body: CreateWorkspaceRequest,
+  ) {
+    const data = await this.handler.execute({
+      ...body,
+      sub,
+    });
+
+    if (data.isLeft()) {
+      return ErrorPresenter.toHTTP(data.value);
+    }
+
+    return {
+      data: WorkspacePresenter.toHTTP(data.value.workspace),
+    };
+  }
+}
