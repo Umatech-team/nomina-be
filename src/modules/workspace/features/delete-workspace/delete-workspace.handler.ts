76a824a6ad5bb9f1978d375b37bc31c48ab7c331
Luan Victor <lvictor.dutra@gmail.com>
2026-02-28 13:11:21 -0300

    feat(workspace): implement DeleteWorkspaceHandler for handling workspace deletion

---
+++ src/modules/workspace/features/delete-workspace/delete-workspace.handler.ts
@@ -0,0 +1,33 @@
+import { WorkspaceRepository } from '@modules/workspace/repositories/contracts/WorkspaceRepository';
+import { HttpException, Injectable } from '@nestjs/common';
+import { Service } from '@shared/core/contracts/Service';
+import { Either, left, right } from '@shared/core/errors/Either';
+import { statusCode } from '@shared/core/types/statusCode';
+import { DeleteWorkspaceRequest } from './delete-workspace.dto';
+
+type Request = DeleteWorkspaceRequest;
+
+type Errors = HttpException;
+
+type Response = null;
+
+@Injectable()
+export class DeleteWorkspaceHandler
+  implements Service<Request, Errors, Response>
+{
+  constructor(private readonly workspaceRepository: WorkspaceRepository) {}
+
+  async execute({ workspaceId }: Request): Promise<Either<Errors, Response>> {
+    const workspace = await this.workspaceRepository.findById(workspaceId);
+
+    if (!workspace) {
+      return left(
+        new HttpException('Workspace not found', statusCode.NOT_FOUND),
+      );
+    }
+
+    await this.workspaceRepository.delete(workspaceId);
+
+    return right(null);
+  }
+}
