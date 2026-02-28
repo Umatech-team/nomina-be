3bb42e378ab6c99273175954f1c2481d66448c1e
Luan Victor <lvictor.dutra@gmail.com>
2026-02-28 13:10:22 -0300

    refactor(workspace): remove unused gateway and service files for code cleanup

--- src/modules/workspace/gateways/FindWorkspace.gateway.ts
+++
@@ -1,8 +0,0 @@
-import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
-import { z } from 'zod';
-
-const findWorkspaceSchema = z.object({
-  workspaceId: z.string().uuid('ID do workspace inválido'),
-});
-
-export const FindWorkspaceGateway = new ZodValidationPipe(findWorkspaceSchema);
