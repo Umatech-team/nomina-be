a98d918bc1b53bcbd29f62412f7e6b261ce3f14d
Luan Victor <lvictor.dutra@gmail.com>
2026-02-28 22:43:18 -0300

    feat(category): add ListCategoriesPipe and schema for list categories validation

---
+++ src/modules/category/features/list-categories/list-categories.dto.ts
@@ -0,0 +1,14 @@
+import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
+import { z } from 'zod';
+
+const listCategoriesSchema = z.object({
+  page: z.coerce.number().int().positive('Página deve ser um número positivo'),
+  pageSize: z.coerce
+    .number()
+    .int()
+    .positive('Tamanho da página deve ser um número positivo')
+    .max(100, 'Tamanho da página muito grande'),
+});
+
+export const ListCategoriesPipe = new ZodValidationPipe(listCategoriesSchema);
+export type ListCategoriesRequest = z.infer<typeof listCategoriesSchema>;
