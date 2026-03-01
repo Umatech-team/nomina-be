4a41e317840a3d89adf50fa134537510bfa4ab61
Luan Victor <lvictor.dutra@gmail.com>
2026-03-01 14:07:46 -0300

    fix(RecurringTransactionRepository): update findManyByWorkspaceId to filter only active transactions

--- src/infra/databases/drizzle/repositories/recurring-transaction.repository.ts
+++ src/infra/databases/drizzle/repositories/recurring-transaction.repository.ts
@@ -64,7 +64,7 @@ export class RecurringTransactionRepositoryImplementation
 
     const baseConditions = and(
       eq(schema.recurringTransactions.workspaceId, workspaceId),
-      isActive !== undefined
+      isActive === true
         ? eq(schema.recurringTransactions.active, isActive)
         : undefined,
     );
