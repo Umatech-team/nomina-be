8d9f97a9b0667e6d84d590a9fd4d77b4653b9bb5
Luan Victor <lvictor.dutra@gmail.com>
2026-02-28 21:17:47 -0300

    feat(workspace): add controller for removing users from workspace

---
+++ src/modules/workspace/features/remove-user-from-workspace/remove-user-from-workspace.controller.ts
@@ -0,0 +1,33 @@
+import { UserRole } from '@constants/enums';
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { Controller, Delete, HttpCode, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Roles } from '@providers/auth/decorators/Roles.decorator';
+import { statusCode } from '@shared/core/types/statusCode';
+import {
+  RemoveWorkspacePipe,
+  RemoveWorkspaceRequest,
+} from './remove-user-from-workspace.dto';
+import { RemoveUserFromWorkspaceHandler } from './remove-user-from-workspace.handler';
+
+@ApiTags('Workspace')
+@Controller('workspace')
+@Roles(UserRole.OWNER, UserRole.ADMIN, UserRole.USER)
+export class RemoveUserFromWorkspaceController {
+  constructor(private readonly handler: RemoveUserFromWorkspaceHandler) {}
+
+  @Delete(':workspaceId/users/:userId')
+  @HttpCode(statusCode.NO_CONTENT)
+  async handle(
+    @Param(RemoveWorkspacePipe) { workspaceId, userId }: RemoveWorkspaceRequest,
+  ) {
+    const data = await this.handler.execute({
+      workspaceId,
+      userId,
+    });
+
+    if (data.isLeft()) {
+      return ErrorPresenter.toHTTP(data.value);
+    }
+  }
+}
