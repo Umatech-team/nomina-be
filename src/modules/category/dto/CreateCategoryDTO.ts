b8ab87de5fd99e139fd54bbd39bb271c139e31fe
Luan Victor <lvictor.dutra@gmail.com>
2026-02-28 22:43:05 -0300

    feat(category): remove category controllers, DTOs, gateways, and services

--- src/modules/category/dto/CreateCategoryDTO.ts
+++
@@ -1,7 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { CategoryDTO } from './CategoryDTO';
-
-export class CreateCategoryDTO extends OmitType(CategoryDTO, [
-  'id',
-  'workspaceId',
-]) {}
--- src/modules/category/gateways/FindCategory.gateway.ts
+++
@@ -1,8 +0,0 @@
-import { ZodValidationPipe } from '@shared/pipes/ZodValidation';
-import { z } from 'zod';
-
-const findCategorySchema = z.object({
-  categoryId: z.string().uuid('ID da categoria inválido'),
-});
-
-export const FindCategoryGateway = new ZodValidationPipe(findCategorySchema);
