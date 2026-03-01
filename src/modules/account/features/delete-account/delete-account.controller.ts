dc49ec95c06b082cece0b0985dd0dca13e7529d2
Luan Victor <lvictor.dutra@gmail.com>
2026-02-28 21:47:58 -0300

    feat(account): add DeleteAccountController for account deletion functionality

---
+++ src/modules/account/features/delete-account/delete-account.controller.ts
@@ -0,0 +1,33 @@
+import { UserRole } from '@constants/enums';
+import { ErrorPresenter } from '@infra/presenters/Error.presenter';
+import { Controller, Delete, HttpCode, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
+import { Roles } from '@providers/auth/decorators/Roles.decorator';
+import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { statusCode } from '@shared/core/types/statusCode';
+import { DeleteAccountPipe, DeleteAccountRequest } from './delete-account.dto';
+import { DeleteAccountHandler } from './delete-account.handler';
+
+@ApiTags('Account')
+@Controller('account')
+@Roles(UserRole.OWNER)
+export class DeleteAccountController {
+  constructor(private readonly handler: DeleteAccountHandler) {}
+
+  @Delete(':id')
+  @HttpCode(statusCode.NO_CONTENT)
+  async handle(
+    @CurrentLoggedUser() { workspaceId }: TokenPayloadSchema,
+    @Param(DeleteAccountPipe) { accountId }: DeleteAccountRequest,
+  ) {
+    const result = await this.handler.execute({
+      accountId,
+      workspaceId,
+    });
+
+    if (result.isLeft()) {
+      return ErrorPresenter.toHTTP(result.value);
+    }
+  }
+}
