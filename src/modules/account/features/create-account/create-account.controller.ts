ef3c1d899325a8710e44fa8dda51a83695d986e9
Luan Victor <lvictor.dutra@gmail.com>
2026-03-05 19:35:40 -0300

    chore: update import statements to use type annotations for CreateAccountRequest

--- src/modules/account/features/create-account/create-account.controller.ts
+++ src/modules/account/features/create-account/create-account.controller.ts
@@ -9,9 +9,12 @@ import { ApiTags } from '@nestjs/swagger';
 import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
 import { Roles } from '@providers/auth/decorators/Roles.decorator';
 import { RolesGuard } from '@providers/auth/guards/Roles.guard';
-import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { type TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
 import { statusCode } from '@shared/core/types/statusCode';
-import { CreateAccountPipe, CreateAccountRequest } from './create-account.dto';
+import {
+  CreateAccountPipe,
+  type CreateAccountRequest,
+} from './create-account.dto';
 import { CreateAccountHandler } from './create-account.handler';
 
 @ApiTags('Account')
