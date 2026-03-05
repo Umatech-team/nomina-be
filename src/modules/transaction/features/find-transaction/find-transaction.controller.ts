868ecf617ae1ccdd714fb3af5c428884e9f84423
Luan Victor <lvictor.dutra@gmail.com>
2026-03-05 19:37:31 -0300

    chore: update import statements to use type annotations for TokenPayloadSchema and FindTransactionRequest

--- src/modules/transaction/features/find-transaction/find-transaction.controller.ts
+++ src/modules/transaction/features/find-transaction/find-transaction.controller.ts
@@ -3,11 +3,11 @@ import { TransactionPresenter } from '@modules/transaction/presenters/Transactio
 import { Controller, Get, HttpCode, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
-import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { type TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
 import { statusCode } from '@shared/core/types/statusCode';
 import {
   FindTransactionPipe,
-  FindTransactionRequest,
+  type FindTransactionRequest,
 } from './find-transaction.dto';
 import { FindTransactionByIdHandler } from './find-transaction.handle';
 
