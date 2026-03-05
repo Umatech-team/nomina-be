cf87148addad79273866b123746a760385e0b245
Luan Victor <lvictor.dutra@gmail.com>
2026-03-05 19:37:21 -0300

    chore: update import statements to use type annotations for TokenPayloadSchema and FindRecurringTransactionRequest

--- src/modules/transaction/features/find-recurring-transaction/find-recurring-transaction.controller.ts
+++ src/modules/transaction/features/find-recurring-transaction/find-recurring-transaction.controller.ts
@@ -3,11 +3,11 @@ import { RecurringTransactionPresenter } from '@modules/transaction/presenters/R
 import { Body, Controller, Get, HttpCode } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
-import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { type TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
 import { statusCode } from '@shared/core/types/statusCode';
 import {
   FindRecurringTransactionPipe,
-  FindRecurringTransactionRequest,
+  type FindRecurringTransactionRequest,
 } from './find-recurring-transaction.dto';
 import { FindRecurringTransactionHandler } from './find-recurring-transaction.handle';
 
