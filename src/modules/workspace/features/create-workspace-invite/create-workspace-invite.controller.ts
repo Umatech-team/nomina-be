c05c0ad42fec07ad2b1376ad691ffcafb0d3bca9
Luan Victor <lvictor.dutra@gmail.com>
2026-03-05 19:38:53 -0300

    chore: update import statements to use type annotations for CreateWorkspaceInviteRequest and TokenPayloadSchema

--- src/modules/workspace/features/create-workspace-invite/create-workspace-invite.controller.ts
+++ src/modules/workspace/features/create-workspace-invite/create-workspace-invite.controller.ts
@@ -11,11 +11,11 @@ import { ApiTags } from '@nestjs/swagger';
 import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
 import { Roles } from '@providers/auth/decorators/Roles.decorator';
 import { RolesGuard } from '@providers/auth/guards/Roles.guard';
-import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { type TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
 import { statusCode } from '@shared/core/types/statusCode';
 import {
   CreateWorkspaceInvitePipe,
-  CreateWorkspaceInviteRequest,
+  type CreateWorkspaceInviteRequest,
 } from './create-workspace-invite.dto';
 import { CreateWorkspaceInviteHandler } from './create-workspace-invite.handler';
 
