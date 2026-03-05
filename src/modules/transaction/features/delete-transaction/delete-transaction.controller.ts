86a8497f4f7d19bef77a5691b31ab2eed815e42f
Luan Victor <lvictor.dutra@gmail.com>
2026-03-05 19:37:14 -0300

    chore: update import statements to use type annotations for TokenPayloadSchema and DeleteTransactionRequest

--- src/modules/transaction/features/delete-transaction/delete-transaction.controller.ts
+++ src/modules/transaction/features/delete-transaction/delete-transaction.controller.ts
@@ -2,11 +2,11 @@ import { ErrorPresenter } from '@infra/presenters/Error.presenter';
 import { Controller, Delete, HttpCode, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CurrentLoggedUser } from '@providers/auth/decorators/CurrentLoggedUser.decorator';
-import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
+import { type TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
 import { statusCode } from '@shared/core/types/statusCode';
 import {
   DeleteTransactionPipe,
-  DeleteTransactionRequest,
+  type DeleteTransactionRequest,
 } from './delete-transaction.dto';
 import { DeleteTransactionHandler } from './delete-transaction.handler';
 
