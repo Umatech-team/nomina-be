aa2bec056226f8ef965f2757d871b70947a360e4
Luan Victor <lvictor.dutra@gmail.com>
2026-03-11 20:49:43 -0300

    refactor: improve formatting and readability in FindTransactionByIdHandler

--- src/modules/transaction/features/find-transaction/find-transaction.handle.ts
+++ src/modules/transaction/features/find-transaction/find-transaction.handle.ts
@@ -15,9 +15,11 @@ type Errors = HttpException;
 type Response = Transaction;
 
 @Injectable()
-export class FindTransactionByIdHandler
-  implements Service<Request, Errors, Response>
-{
+export class FindTransactionByIdHandler implements Service<
+  Request,
+  Errors,
+  Response
+> {
   constructor(
     private readonly transactionRepository: TransactionRepository,
     private readonly userRepository: UserRepository,
