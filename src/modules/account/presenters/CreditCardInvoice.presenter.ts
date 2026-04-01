604d7df9cdb671033ac06673adc8912f29ceb58d
Luan Victor <lvictor.dutra@gmail.com>
2026-04-01 16:00:28 -0300

    feat(account): add CreditCardInvoice presenter

---
+++ src/modules/account/presenters/CreditCardInvoice.presenter.ts
@@ -0,0 +1,27 @@
+import { Account } from '@modules/account/entities/Account';
+import { AccountPresenter } from '@modules/account/presenters/Account.presenter';
+import { Transaction } from '@modules/transaction/entities/Transaction';
+import { TransactionPresenter } from '@modules/transaction/presenters/Transaction.presenter';
+import { MoneyUtils } from '@utils/MoneyUtils';
+
+interface CreditCardInvoiceData {
+  account: Account;
+  transactions: Transaction[];
+  totalAmount: number;
+  dueDate: Date;
+  periodStart: Date;
+  periodEnd: Date;
+}
+
+export class CreditCardInvoicePresenter {
+  static toHTTP(data: CreditCardInvoiceData) {
+    return {
+      account: AccountPresenter.toHTTP(data.account),
+      transactions: data.transactions.map(TransactionPresenter.toHTTP),
+      totalAmount: MoneyUtils.centsToDecimal(data.totalAmount),
+      dueDate: data.dueDate.toISOString(),
+      periodStart: data.periodStart.toISOString(),
+      periodEnd: data.periodEnd.toISOString(),
+    };
+  }
+}
