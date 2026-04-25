f7be51ea0dc2fe460db33e1ef0e33384017cb45f
Luan Victor <lvictor.dutra@gmail.com>
2026-04-25 14:01:16 -0300

    refactor(account): rename FindAccountByIdHandler to FindAccountByIdService and update error handling

--- src/modules/account/features/find-account/find-account.handler.ts
+++ src/modules/account/features/find-account/find-account.handler.ts
@@ -1,30 +1,30 @@
-import { Account } from '@modules/account/entities/Account';
+import { AnyAccount } from '@modules/account/entities/types';
 import { AccountRepository } from '@modules/account/repositories/contracts/AccountRepository';
-import { HttpException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { TokenPayloadSchema } from '@providers/auth/strategys/jwtStrategy';
 import { Service } from '@shared/core/contracts/Service';
 import { Either, left, right } from '@shared/core/errors/Either';
-import { statusCode } from '@shared/core/types/statusCode';
 import { FindAccountRequest } from './find-account.dto';
+import { UnauthorizedError } from '@shared/errors/UnauthorizedError';
 
 type Request = FindAccountRequest & Pick<TokenPayloadSchema, 'workspaceId'>;
-type Errors = HttpException;
-type Response = Account;
 
 @Injectable()
-export class FindAccountByIdHandler
-  implements Service<Request, Errors, Response>
-{
+export class FindAccountByIdService implements Service<
+  Request,
+  Error,
+  AnyAccount
+> {
   constructor(private readonly accountRepository: AccountRepository) {}
 
   async execute({
     accountId,
     workspaceId,
-  }: Request): Promise<Either<Errors, Response>> {
+  }: Request): Promise<Either<Error, AnyAccount>> {
     const account = await this.accountRepository.findById(accountId);
 
-    if (!account || account.workspaceId !== workspaceId) {
-      return left(new HttpException('account not found', statusCode.NOT_FOUND));
+    if (account?.workspaceId !== workspaceId) {
+      return left(new UnauthorizedError());
     }
 
     return right(account);
