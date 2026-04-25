d887297a555ebcfa630c580fa2ffc8492ecb103e
Luan Victor <lvictor.dutra@gmail.com>
2026-04-25 13:58:57 -0300

    feat(account): add BaseAccount entity with properties and methods

---
+++ src/modules/account/entities/BaseAccount.ts
@@ -0,0 +1,34 @@
+import { AggregateRoot } from '@shared/core/Entities/AggregateRoot';
+
+export interface BaseAccountProps {
+  workspaceId: string;
+  name: string;
+  timezone: string;
+}
+
+export abstract class BaseAccount<
+  T extends BaseAccountProps,
+> extends AggregateRoot<T> {
+  protected constructor(props: T, id?: string) {
+    super(props, id);
+  }
+
+  get workspaceId(): string {
+    return this.props.workspaceId;
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  public updateName(name: string) {
+    if (!name || typeof name !== 'string' || name.trim().length === 0) {
+      throw new Error('Nome inválido.');
+    }
+    this.props.name = name.trim();
+  }
+
+  abstract get patrimonyContribution(): bigint;
+
+  abstract get type(): string;
+}
