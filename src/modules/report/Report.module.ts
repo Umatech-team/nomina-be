3a3202d0c64ba9f163d59c3ea4e8a8756e9538a5
Luan Victor <lvictor.dutra@gmail.com>
2026-04-26 14:04:56 -0300

    refactor(ReportModule): reorder imports for improved organization and clarity

--- src/modules/report/Report.module.ts
+++ src/modules/report/Report.module.ts
@@ -1,13 +1,13 @@
 import { DatabaseModule } from '@infra/databases/Database.module';
 import { Module } from '@nestjs/common';
 import { BalanceEvolutionController } from './features/get-balance-evolution/get-balance-evolution.controller';
-import { CashFlowEvolutionController } from './features/get-cash-flow-evolution/cash-flow-evolution.controller';
-import { GetExpensesByCategoryController } from './features/get-expenses-by-category/get-expenses-by-category.controller';
-import { FindMonthSummaryController } from './features/get-month-summary/get-month-summary.controller';
 import { BalanceEvolutionService } from './features/get-balance-evolution/get-balance-evolution.handler';
+import { CashFlowEvolutionController } from './features/get-cash-flow-evolution/cash-flow-evolution.controller';
 import { CashFlowEvolutionService } from './features/get-cash-flow-evolution/cash-flow-evolution.handler';
+import { GetExpensesByCategoryController } from './features/get-expenses-by-category/get-expenses-by-category.controller';
 import { GetExpensesByCategoryService } from './features/get-expenses-by-category/get-expenses-by-category.handler';
-import { FindMonthSummaryService } from './features/get-month-summary/get-month-summary.handler';
+import { FindMonthSummaryController } from './features/get-month-summary/get-month-summary.controller';
+import { FindMonthSummaryService } from './features/get-month-summary/get-month-summary.service';
 
 @Module({
   imports: [DatabaseModule],
