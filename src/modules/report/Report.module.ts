62ada6f80db75713222d945b8ac8afe62cab48d9
Luan Victor <lvictor.dutra@gmail.com>
2026-04-26 13:36:16 -0300

    refactor(report.module): update service imports to use handler files

--- src/modules/report/Report.module.ts
+++ src/modules/report/Report.module.ts
@@ -1,13 +1,13 @@
 import { DatabaseModule } from '@infra/databases/Database.module';
 import { Module } from '@nestjs/common';
 import { BalanceEvolutionController } from './features/get-balance-evolution/get-balance-evolution.controller';
-import { BalanceEvolutionService } from './features/get-balance-evolution/get-balance-evolution.service';
 import { CashFlowEvolutionController } from './features/get-cash-flow-evolution/cash-flow-evolution.controller';
-import { CashFlowEvolutionService } from './features/get-cash-flow-evolution/cash-flow-evolution.service';
 import { GetExpensesByCategoryController } from './features/get-expenses-by-category/get-expenses-by-category.controller';
-import { GetExpensesByCategoryService } from './features/get-expenses-by-category/get-expenses-by-category.service';
 import { FindMonthSummaryController } from './features/get-month-summary/get-month-summary.controller';
-import { FindMonthSummaryService } from './features/get-month-summary/get-month-summary.service';
+import { BalanceEvolutionService } from './features/get-balance-evolution/get-balance-evolution.handler';
+import { CashFlowEvolutionService } from './features/get-cash-flow-evolution/cash-flow-evolution.handler';
+import { GetExpensesByCategoryService } from './features/get-expenses-by-category/get-expenses-by-category.handler';
+import { FindMonthSummaryService } from './features/get-month-summary/get-month-summary.handler';
 
 @Module({
   imports: [DatabaseModule],
