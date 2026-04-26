0228ca3d25ee7ffa2f74f212683491cb63ac2283
Luan Victor <lvictor.dutra@gmail.com>
2026-04-26 13:06:20 -0300

    refactor(update-workspace.controller): adjust import formatting for consistency

--- src/modules/workspace/features/update-workspace/update-workspace.controller.ts
+++ src/modules/workspace/features/update-workspace/update-workspace.controller.ts
@@ -2,20 +2,20 @@ import { UserRole } from '@constants/enums';
 import { ErrorPresenter } from '@infra/presenters/ErrorPresenter';
 import { WorkspacePresenter } from '@modules/workspace/presenters/Workspace.presenter';
 import {
-    Body,
-    Controller,
-    HttpCode,
-    Param,
-    Put,
-    UseGuards,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Roles } from '@providers/auth/decorators/Roles.decorator';
 import { RolesGuard } from '@providers/auth/guards/Roles.guard';
 import { statusCode } from '@shared/core/types/statusCode';
 import {
-    UpdateWorkspacePipe,
-    UpdateWorkspaceRequest,
+  UpdateWorkspacePipe,
+  UpdateWorkspaceRequest,
 } from './update-workspace.dto';
 import { UpdateWorkspaceService } from './update-workspace.service';
 
