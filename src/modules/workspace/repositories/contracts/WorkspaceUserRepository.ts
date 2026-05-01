88ac43d7692da0874da56d7a60086048fa879302
Luan Victor <lvictor.dutra@gmail.com>
2026-05-01 17:21:55 -0300

    refactor(WorkspaceUserRepository): add abstract method for setting default workspace

--- src/modules/workspace/repositories/contracts/WorkspaceUserRepository.ts
+++ src/modules/workspace/repositories/contracts/WorkspaceUserRepository.ts
@@ -1,6 +1,11 @@
 import { WorkspaceUser } from '@modules/workspace/entities/WorkspaceUser';
 
 export abstract class WorkspaceUserRepository {
+  abstract setDefaultWorkspace(
+    userId: string,
+    workspaceId: string,
+  ): Promise<void>;
+
   abstract findDefaultWorkspaceByUserId(userId: string): Promise<{
     user: WorkspaceUser;
     workspaceName: string;
