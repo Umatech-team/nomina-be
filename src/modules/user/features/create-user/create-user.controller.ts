693fe923acf1fc61d93da35eb469ad146d401f09
Luan Victor <lvictor.dutra@gmail.com>
2026-05-03 10:42:26 -0300

    fix(CreateUserController): add throttling to handle method

--- src/modules/user/features/create-user/create-user.controller.ts
+++ src/modules/user/features/create-user/create-user.controller.ts
@@ -2,6 +2,7 @@ import { ErrorPresenter } from '@infra/presenters/ErrorPresenter';
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Public } from '@providers/auth/decorators/IsPublic.decorator';
+import { Throttle } from '@nestjs/throttler';
 import { statusCode } from '@shared/core/types/statusCode';
 import { CreateUserPipe, type CreateUserRequest } from './create-user.dto';
 import { CreateUserService } from './create-user.service';
@@ -12,6 +13,7 @@ export class CreateUserController {
   constructor(private readonly service: CreateUserService) {}
 
   @Public()
+  @Throttle({ auth: { limit: 10, ttl: 60_000 } })
   @Post()
   @HttpCode(statusCode.CREATED)
   async handle(@Body(CreateUserPipe) body: CreateUserRequest) {
