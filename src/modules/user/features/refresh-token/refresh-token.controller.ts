2acfbaa7cb93bad03fcc4fd8ebdcf382d1641486
Luan Victor <lvictor.dutra@gmail.com>
2026-05-03 10:42:40 -0300

    fix(RefreshTokenController): add throttling to refresh session method

--- src/modules/user/features/refresh-token/refresh-token.controller.ts
+++ src/modules/user/features/refresh-token/refresh-token.controller.ts
@@ -1,6 +1,7 @@
 import { ErrorPresenter } from '@infra/presenters/ErrorPresenter';
 import { Controller, Get, HttpCode } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Throttle } from '@nestjs/throttler';
 import { Public } from '@providers/auth/decorators/IsPublic.decorator';
 import { RefreshToken } from '@providers/auth/decorators/refreshToken.decorator';
 import { statusCode } from '@shared/core/types/statusCode';
@@ -12,6 +13,7 @@ export class RefreshTokenController {
   constructor(private readonly service: RefreshTokenService) {}
 
   @Public()
+  @Throttle({ auth: { limit: 10, ttl: 60_000 } })
   @Get('/session/refresh')
   @HttpCode(statusCode.OK)
   async handle(@RefreshToken() refreshToken: string) {
